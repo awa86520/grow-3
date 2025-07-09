9a6dec1e2aa80371d04bf28150dc24c28a72e562
awanish kumar yadav <aay14155@gmail.com>
2025-07-09 19:20:24 +0530

    d

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ const userRoutes = require('./routes/userRoutes');
   const app = express(); 
    app,use( express.json());
    connectDB();
-   app.uue('/api',userRoutes);
-   app.listen(500);
+   app.use('/api',userRoutes);
+   const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
